2b3082df13622016a19e59755d35b1ef68513499
mikechoi1 <befuddledmike@gmail.com>
2019-05-08 17:29:51 -0700

    Updates

---
+++ precache-manifest.f4990f6504260b2f1f9be44f3b4ebc38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e506b84dbbd43aee84c",
+    "url": "/nailpalacescv.com/static/css/main.b3acc589.chunk.css"
+  },
+  {
+    "revision": "1e506b84dbbd43aee84c",
+    "url": "/nailpalacescv.com/static/js/main.254a5963.chunk.js"
+  },
+  {
+    "revision": "fee0a244b3df616f9284",
+    "url": "/nailpalacescv.com/static/js/runtime~main.f3bd4ebf.js"
+  },
+  {
+    "revision": "cbe0c17e69a897b8916f",
+    "url": "/nailpalacescv.com/static/js/2.516876f8.chunk.js"
+  },
+  {
+    "revision": "a90f0c7f2dd24c8a2856c97ad46da042",
+    "url": "/nailpalacescv.com/index.html"
+  }
+];
