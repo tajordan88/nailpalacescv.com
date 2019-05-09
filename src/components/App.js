ab541dad2a0126e7c75b1e4168cb2f29807c8479
mikechoi1 <befuddledmike@gmail.com>
2019-05-09 11:05:48 -0700

    Fixed static images, removed BrowserRouter for gh pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
 import Header from './header';
 import Home from './home';
-
-const aboutpage = () => {
-    return (
-        <div>
-            This is the about page.
-        </div>
-    );
-};
 
 const App = () => {
     return (
         <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route path="/" exact component={Home} />
-                    <Route path="/about" exact component={aboutpage} />
-                </div>
-            </BrowserRouter>
+            <Header />
+            <Home />
         </div>
     );
 };
